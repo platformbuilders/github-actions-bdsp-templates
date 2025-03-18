65d19aa3d528f8d113c2e1745b22bf06d295e637
Gerffeson <gerffeson.mota@platformbuilders.io>
2025-03-18 11:54:07 -0300

    Updates

--- build-push-gcr/entrypoint.sh
+++ build-push-gcr/entrypoint.sh
@@ -2,22 +2,13 @@
 
 set -e
 
-GCP_SERVICE_ACCOUNT_KEY=$1
-
-# Decode service account key
-echo "$GCP_SERVICE_ACCOUNT_KEY" | base64 -d > gcp-service-account.json
-
-# Authenticate with GCP
-gcloud auth activate-service-account --key-file=gcp-service-account.json
+echo "GITHUB_REF_NAME: $GITHUB_REF_NAME"
 
 # Get short SHA
 SHORT_SHA=$(git rev-parse --short=7 HEAD)
 
 # Build and Push Docker image
-REPOSITORY_NAME=$(basename "$REPOSITORY_NAME")
-
-echo "GITHUB_REF_NAME: $GITHUB_REF_NAME"
-echo "REPOSITORY_NAME: $REPOSITORY_NAME"
+REPOSITORY_NAME=$(basename "$GITHUB_REPOSITORY")
 
 if [[ "$GITHUB_REF_NAME" == "staging" ]]; then
   REPOSITORY_URI="us-docker.pkg.dev/image-registry-326015/$REPOSITORY_NAME/staging"
@@ -38,6 +29,3 @@ IMAGE_TAG="$SHORT_SHA"
 # Output image tag and digest
 echo "IMAGE_TAG=$IMAGE_TAG" >> "$GITHUB_OUTPUT"
 echo "IMAGE_DIGEST=$IMAGE_DIGEST" >> "$GITHUB_OUTPUT"
-
-# Clean up service account key
-rm gcp-service-account.json
