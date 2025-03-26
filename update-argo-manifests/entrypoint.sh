d7d68bf4748d33f07f72aa57658041441cc40db2
Gerffeson <gerffeson.mota@platformbuilders.io>
2025-03-26 14:14:39 -0300

    Ajustes novos fluxo prd

--- update-argo-manifests/entrypoint.sh
+++ update-argo-manifests/entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-IMAGE_TAG=$1
-IMAGE_DIGEST=$2
-GITHUB_TOKEN=$3
-REPOSITORY_NAME=$4
+IMAGE_TAG="$1"
+IMAGE_DIGEST="$2"
+GITHUB_TOKEN="$3"
+REPOSITORY_NAME="$4"
 
 echo "IMAGE_TAG: $IMAGE_TAG"
 echo "IMAGE_DIGEST: $IMAGE_DIGEST"
@@ -16,14 +16,14 @@ echo "GITHUB_REF_NAME: $GITHUB_REF_NAME"
 echo "REPOSITORY_NAME: $REPOSITORY_NAME"
 
 # Determinar o Target Repository
-if [[ "${GITHUB_REF_NAME}" == "staging" || "${GITHUB_REF_NAME}" =~ ^release/ || "${GITHUB_REF_NAME}" == "homolog"  ]]; then
+if [[ "$GITHUB_REF_NAME" == "staging" || "$GITHUB_REF_NAME" =~ ^release/ || "$GITHUB_REF_NAME" == "homolog" ]]; then
   TARGET_REPO="${REPOSITORY_NAME}-hml"
-elif [[ "${GITHUB_REF_NAME}" == "develop" ]]; then
+elif [[ "$GITHUB_REF_NAME" == "develop" ]]; then
   TARGET_REPO="${REPOSITORY_NAME}-dev"
-elif [[ "${GITHUB_REF_NAME}" == "master" || "${GITHUB_REF_NAME}" == "main" ]]; then
+elif [[ "$GITHUB_REF_NAME" == "master" || "$GITHUB_REF_NAME" == "main" ]]; then
   TARGET_REPO="${REPOSITORY_NAME}-prd"
 else
-  echo "Branch não suportada: ${GITHUB_REF_NAME}"
+  echo "Branch não suportada: $GITHUB_REF_NAME"
   exit 1
 fi
 
@@ -34,39 +34,38 @@ GIT_CLONE_COMMAND="git clone https://${GITHUB_TOKEN}@github.com/platformbuilders
 echo "Executing: $GIT_CLONE_COMMAND"
 $GIT_CLONE_COMMAND
 
-# Encontrar o arquivo de deployment
-DEPLOYMENT_FILE=$(find argo-manifests/ -name "*-dp.yaml")
-
-if [[ -z "$DEPLOYMENT_FILE" ]]; then
-  echo "Erro: Arquivo de deployment não encontrado."
-  exit 1
-fi
-
-echo "Arquivo de deployment encontrado: $DEPLOYMENT_FILE"
-
 cd argo-manifests
 
-if [[ "${GITHUB_REF_NAME}" == "master" || "${GITHUB_REF_NAME}" == "main" ]]; then
-  # Trabalhar na branch dev
+if [[ "$GITHUB_REF_NAME" == "master" || "$GITHUB_REF_NAME" == "main" ]]; then
+  # Checkout do branch dev ANTES de encontrar o arquivo
   git checkout dev
+
+  # Encontrar o arquivo de deployment
+  DEPLOYMENT_FILE=$(find . -name "*-dp.yaml")
+
+  if [[ -z "$DEPLOYMENT_FILE" ]]; then
+    echo "Erro: Arquivo de deployment não encontrado no branch dev."
+    exit 1
+  fi
+
+  echo "Arquivo de deployment encontrado: $DEPLOYMENT_FILE"
 
   # Atualizar o arquivo YAML
 
   # Atualizar tags.datadoghq.com/version em metadata.labels
-  sed -i "s/\(tags.datadoghq.com\/version: \)\"[^\"]*\"/\1\"$IMAGE_TAG\"/g" $DEPLOYMENT_FILE
+  sed -i "s/\(tags.datadoghq.com\/version: \)\"[^\"]*\"/\1\"$IMAGE_TAG\"/g" "$DEPLOYMENT_FILE"
 
   # Atualizar tags.datadoghq.com/version em template.metadata.labels
-  sed -i "s/\(tags.datadoghq.com\/version: \)\"[^\"]*\"/\1\"$IMAGE_TAG\"/g" $DEPLOYMENT_FILE
+  sed -i "s/\(tags.datadoghq.com\/version: \)\"[^\"]*\"/\1\"$IMAGE_TAG\"/g" "$DEPLOYMENT_FILE"
 
   # Atualizar image
-  sed -i "s|\(image: us-docker.pkg.dev/image-registry-326015/${REPOSITORY_NAME}/${GITHUB_REF_NAME}@\)[^ ]*|\1${IMAGE_DIGEST}|g" $DEPLOYMENT_FILE
+  sed -i "s|\(image: us-docker.pkg.dev/image-registry-326015/${REPOSITORY_NAME}/${GITHUB_REF_NAME}@\)[^ ]*|\1$IMAGE_DIGEST|g" "$DEPLOYMENT_FILE"
 
-  # Commit e push ou abrir PR
-  
+  # Commit e push
   git config --local user.email "actions@github.com"
   git config --local user.name "GitHub Actions"
-  git add "$(basename $DEPLOYMENT_FILE)"
-  git commit -m "Update deployment with image: ${IMAGE_TAG}"
+  git add "$(basename "$DEPLOYMENT_FILE")"
+  git commit -m "Update deployment with image: $IMAGE_TAG"
   git push origin dev
 
   # Verificar se há mudanças entre dev e master
@@ -74,34 +73,44 @@ if [[ "${GITHUB_REF_NAME}" == "master" || "${GITHUB_REF_NAME}" == "main" ]]; the
     echo "Nenhuma diferença entre master e dev. Nenhum PR será criado."
     exit 0
   fi
-    
+
   # Criar PR da dev -> master
   echo "Alterações detectadas! Criando Pull Request..."
-  gh pr create --title "Update deployment with image: ${IMAGE_TAG}" \
-               --body "Update deployment with image: ${IMAGE_TAG}" \
+  gh pr create --title "Update deployment with image: $IMAGE_TAG" \
+               --body "Update deployment with image: $IMAGE_TAG" \
                --base master \
                --head dev
 
-elif [[ "${GITHUB_REF_NAME}" == "staging" || "${GITHUB_REF_NAME}" =~ ^release/ || "${GITHUB_REF_NAME}" == "homolog" || "${GITHUB_REF_NAME}" == "develop"  ]]; then
+elif [[ "$GITHUB_REF_NAME" == "staging" || "$GITHUB_REF_NAME" =~ ^release/ || "$GITHUB_REF_NAME" == "homolog" || "$GITHUB_REF_NAME" == "develop" ]]; then
   git checkout master
+
+  # Encontrar o arquivo de deployment
+  DEPLOYMENT_FILE=$(find . -name "*-dp.yaml")
+
+  if [[ -z "$DEPLOYMENT_FILE" ]]; then
+    echo "Erro: Arquivo de deployment não encontrado no branch master."
+    exit 1
+  fi
+
+  echo "Arquivo de deployment encontrado: $DEPLOYMENT_FILE"
 
   # Atualizar o arquivo YAML
 
   # Atualizar tags.datadoghq.com/version em metadata.labels
-  sed -i "s/\(tags.datadoghq.com\/version: \)\"[^\"]*\"/\1\"$IMAGE_TAG\"/g" $DEPLOYMENT_FILE
+  sed -i "s/\(tags.datadoghq.com\/version: \)\"[^\"]*\"/\1\"$IMAGE_TAG\"/g" "$DEPLOYMENT_FILE"
 
   # Atualizar tags.datadoghq.com/version em template.metadata.labels
-  sed -i "s/\(tags.datadoghq.com\/version: \)\"[^\"]*\"/\1\"$IMAGE_TAG\"/g" $DEPLOYMENT_FILE
+  sed -i "s/\(tags.datadoghq.com\/version: \)\"[^\"]*\"/\1\"$IMAGE_TAG\"/g" "$DEPLOYMENT_FILE"
 
   # Atualizar image
-  sed -i "s|\(image: us-docker.pkg.dev/image-registry-326015/${REPOSITORY_NAME}/${GITHUB_REF_NAME}@\)[^ ]*|\1${IMAGE_DIGEST}|g" $DEPLOYMENT_FILE
+  sed -i "s|\(image: us-docker.pkg.dev/image-registry-326015/${REPOSITORY_NAME}/${GITHUB_REF_NAME}@\)[^ ]*|\1$IMAGE_DIGEST|g" "$DEPLOYMENT_FILE"
 
-  
+  # Commit e push
   git config --local user.email "actions@github.com"
   git config --local user.name "GitHub Actions"
-  git add "$(basename $DEPLOYMENT_FILE)"
-  git commit -m "Update deployment with image: ${IMAGE_TAG}"
+  git add "$(basename "$DEPLOYMENT_FILE")"
+  git commit -m "Update deployment with image: $IMAGE_TAG"
   git push origin master
 else
-  echo "Nenhuma ação necessária para a branch ${GITHUB_REF_NAME}"
+  echo "Nenhuma ação necessária para a branch $GITHUB_REF_NAME"
 fi
