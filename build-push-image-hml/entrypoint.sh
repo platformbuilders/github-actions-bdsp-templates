f8b8227afb794cc3f3e2d08470efa87c5d77a23a
marcos-platform-builders <marcos.aurelio@platformbuilders.io>
2025-06-10 15:48:55 -0300

    remove production ci

--- build-push-image-hml/entrypoint.sh
+++ build-push-image-hml/entrypoint.sh
@@ -49,64 +49,64 @@ gcloud auth activate-service-account --key-file=gcp-sa.json
 gcloud auth configure-docker us-docker.pkg.dev
 
 # Verificar se a branch é master ou main
-if [[ "$GITHUB_REF_NAME" == "master" || "$GITHUB_REF_NAME" == "main" ]]; then
+# #if [[ "$GITHUB_REF_NAME" == "master" || "$GITHUB_REF_NAME" == "main" ]]; then
 
-  if [[ "$PROJECT_TYPE" == "frontend" ]]; then
-      echo "Branch master/main detectada para projeto frontend. Realizando build e push..."
-      docker build -t "$REPOSITORY_URI_PRD":"$SHORT_SHA" .
+  # if [[ "$PROJECT_TYPE" == "frontend" ]]; then
+      # echo "Branch master/main detectada para projeto frontend. Realizando build e push..."
+      # docker build -t "$REPOSITORY_URI_PRD":"$SHORT_SHA" .
 
-      TAG_EXISTS=$(gcloud artifacts docker tags list "$REPOSITORY_URI_BRANCH" \
-      --filter="tag~'$SHORT_SHA'" \
-      --format="value(tag)" 2>/dev/null || true)
+      # TAG_EXISTS=$(gcloud artifacts docker tags list "$REPOSITORY_URI_BRANCH" \
+      # --filter="tag~'$SHORT_SHA'" \
+      # --format="value(tag)" 2>/dev/null || true)
 
-      if [[ -n "$TAG_EXISTS" ]]; then
-        echo "Tag '$SHORT_SHA' já existe em $REPOSITORY_URI_BRANCH. Deletando..."
-        gcloud artifacts docker tags delete "$REPOSITORY_URI_BRANCH:$SHORT_SHA" --quiet || true
-      fi
+      # if [[ -n "$TAG_EXISTS" ]]; then
+        # echo "Tag '$SHORT_SHA' já existe em $REPOSITORY_URI_BRANCH. Deletando..."
+        # gcloud artifacts docker tags delete "$REPOSITORY_URI_BRANCH:$SHORT_SHA" --quiet || true
+      # fi
 
-      docker push "$REPOSITORY_URI_PRD":"$SHORT_SHA"
+      # docker push "$REPOSITORY_URI_PRD":"$SHORT_SHA"
 
-      IMAGE_DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' "$REPOSITORY_URI_PRD":"$SHORT_SHA" | cut -d '@' -f 2)
-      IMAGE_TAG="$SHORT_SHA" 
-      IMAGE_URI="$REPOSITORY_URI_BRANCH:$SHORT_SHA"
+      # IMAGE_DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' "$REPOSITORY_URI_PRD":"$SHORT_SHA" | cut -d '@' -f 2)
+      # IMAGE_TAG="$SHORT_SHA" 
+      # IMAGE_URI="$REPOSITORY_URI_BRANCH:$SHORT_SHA"
 
-      echo "IMAGE_TAG=$IMAGE_TAG" >> "$GITHUB_OUTPUT"
-      echo "IMAGE_DIGEST=$IMAGE_DIGEST" >> "$GITHUB_OUTPUT"
-      echo "IMAGE_URI=$IMAGE_URI" >> "$GITHUB_OUTPUT"
-      echo "Outputs definidos"
+      # echo "IMAGE_TAG=$IMAGE_TAG" >> "$GITHUB_OUTPUT"
+      # echo "IMAGE_DIGEST=$IMAGE_DIGEST" >> "$GITHUB_OUTPUT"
+      # echo "IMAGE_URI=$IMAGE_URI" >> "$GITHUB_OUTPUT"
+      # echo "Outputs definidos"
 
-      echo "Build e push concluídos para frontend em $GITHUB_REF_NAME."
+      # echo "Build e push concluídos para frontend em $GITHUB_REF_NAME."
 
-  else
-      LATEST_IMAGE_LINE=$(gcloud artifacts docker images list --include-tags "$REPOSITORY_URI_PRD" \
-          --sort-by=~CREATE_TIME \
-          --limit=1 \
-          --quiet \
-          | tail -n 1)
+  # else
+      # LATEST_IMAGE_LINE=$(gcloud artifacts docker images list --include-tags "$REPOSITORY_URI_PRD" \
+          # --sort-by=~CREATE_TIME \
+          # --limit=1 \
+          # --quiet \
+          # | tail -n 1)
 
-      if [[ -z "$LATEST_IMAGE_LINE" ]]; then
-          echo "Erro Crítico: Nenhuma linha de dados retornada por gcloud."
-          exit 1
-      fi
+      # if [[ -z "$LATEST_IMAGE_LINE" ]]; then
+          # echo "Erro Crítico: Nenhuma linha de dados retornada por gcloud."
+          # exit 1
+      # fi
 
-      IMAGE_DIGEST=$(echo "$LATEST_IMAGE_LINE" | awk '{print $2}')
-      IMAGE_TAG=$(echo "$LATEST_IMAGE_LINE" | awk '{print $3}')
+      # IMAGE_DIGEST=$(echo "$LATEST_IMAGE_LINE" | awk '{print $2}')
+      # IMAGE_TAG=$(echo "$LATEST_IMAGE_LINE" | awk '{print $3}')
 
-      if [[ -z "$IMAGE_TAG" ]] || [[ -z "$IMAGE_DIGEST" ]] || [[ ! "$IMAGE_DIGEST" =~ ^sha256: ]]; then
-         echo "Erro Crítico: Falha ao extrair tag ou digest válido da linha."
-         echo "Linha processada: '$LATEST_IMAGE_LINE'"
-         exit 1
-      fi
+      # if [[ -z "$IMAGE_TAG" ]] || [[ -z "$IMAGE_DIGEST" ]] || [[ ! "$IMAGE_DIGEST" =~ ^sha256: ]]; then
+        #  echo "Erro Crítico: Falha ao extrair tag ou digest válido da linha."
+        #  echo "Linha processada: '$LATEST_IMAGE_LINE'"
+        #  exit 1
+      # fi
 
-        echo "IMAGE_TAG=$IMAGE_TAG" >> "$GITHUB_OUTPUT"
-        echo "IMAGE_DIGEST=$IMAGE_DIGEST" >> "$GITHUB_OUTPUT"
-        echo "Outputs definidos."
+        # echo "IMAGE_TAG=$IMAGE_TAG" >> "$GITHUB_OUTPUT"
+        # echo "IMAGE_DIGEST=$IMAGE_DIGEST" >> "$GITHUB_OUTPUT"
+        # echo "Outputs definidos."
 
-  fi
+  # fi
 
 
 # Verificar se a branch é release/*, staging ou homolog
-elif [[ "$GITHUB_REF_NAME" =~ ^release/ || "$GITHUB_REF_NAME" == "staging" || "$GITHUB_REF_NAME" == "homolog" ]]; then
+if [[ "$GITHUB_REF_NAME" =~ ^release/ || "$GITHUB_REF_NAME" == "staging" || "$GITHUB_REF_NAME" == "homolog" || "$GITHUB_REF_NAME" == "master" || "$GITHUB_REF_NAME" == "main"  ]]; then
 
   if [[ "$PROJECT_TYPE" == "frontend" ]]; then
       echo "Detectado projeto frontend. Realizando build e push..."
